d6d810312d29a73856a9b0be260c036acf567515
Jonathan <jleack@gmail.com>
2018-08-22 19:19:51 -0700

    All main features implemented

--- classes/SoccerLeague.js
+++ classes/SoccerLeague.js
@@ -2,7 +2,8 @@ class SoccerLeague {
     constructor() {
         this.inputString = '',
         this.gameTeams = [],
-        this.gamePoints = []
+        this.gameScores = [],
+        this.teamStandings = []
     }
 
     /*======================================================================
@@ -24,7 +25,7 @@ class SoccerLeague {
     }
 
     /*======================================================================
-    // This method parse the previously created string to find all team
+    // This method parses the previously created string to find all team
     // names and scores, pushing them to arrays within the class.
     ======================================================================*/
     parseForTeamsAndScores() {
@@ -35,7 +36,7 @@ class SoccerLeague {
         // that correspond to scores. Push both of these to arrays within the.
         // class. SkipIndexes is used to skip indexes already taken into
         // consideration in nested loops.
-        for (let i in this.inputString) {
+        for (let i = 0; i < this.inputString.length; i++) {
             if (skipIndexes) {
                 skipIndexes--;
                 continue;
@@ -65,13 +66,90 @@ class SoccerLeague {
                     skipIndexes++;
                 }
 
-                this.gamePoints.push(teamScore);
+                this.gameScores.push(teamScore);
                 continue;
             }
 
         }
     }
+    
+    /*======================================================================
+    // This method parses the gameTeams and gameScores arrays within the
+    // class to calculate total winning points per team. Note that we will
+    // address points in groups of two (2 teams) and skip one index per
+    // run.
+    ======================================================================*/
+    calculateWinsPerTeam() {
+        let skipIndex = 0;
 
+        for (let i = 0; i < this.gameTeams.length; i++) {
+            if (skipIndex) {
+                skipIndex--;
+                continue;
+            }
+
+            // Determine if either or both teams in a game currently have
+            // an object created in teamStandings.
+            if (this.teamStandings.filter((team) => team.name === this.gameTeams[i]) > -1) {
+                this.teamStandings.push({ name: this.gameTeams[i], points: 0 })
+            }
+
+            if (this.teamStandings.filter((team) => team.name === this.gameTeams[i + 1]) > -1) {
+                this.teamStandings.push({ name: this.gameTeams[i + 1], points: 0 })
+            }
+
+            // Determine winner for a game, or tie, and award points. Wins are worth
+            // 3 points while a tie awards both team with 1 point. The findIndexOfTeam
+            // helper method will be called to find corresponding indexes.
+            if (this.gameScores[i] > this.gameScores[i + 1]) {
+                let winningTeamIndex = this.findIndexOfTeam(this.gameTeams[i]);
+                this.teamStandings[winningTeamIndex].points = this.teamStandings[winningTeamIndex].points + 3;
+                skipIndex++;
+            } else if (this.gameScores[i] < this.gameScores[i + 1]) {
+                let winningTeamIndex = this.findIndexOfTeam(this.gameTeams[i + 1]);
+                this.teamStandings[winningTeamIndex].points = this.teamStandings[winningTeamIndex].points + 3;
+                skipIndex++;
+            } else if (this.gameScores[i] === this.gameScores[i + 1]) {
+                let tyingTeamIndex1 = this.findIndexOfTeam(this.gameTeams[i]);
+                let tyingTeamIndex2 = this.findIndexOfTeam(this.gameTeams[i + 1]);
+                this.teamStandings[tyingTeamIndex1].points = this.teamStandings[tyingTeamIndex1].points + 1;
+                this.teamStandings[tyingTeamIndex2].points = this.teamStandings[tyingTeamIndex2].points + 1;
+                skipIndex++;
+            }
+
+        }
+    }
+
+    /*======================================================================
+    // This helper method will loop through the teamStandings array of
+    // objects to find the index of a matching team name.
+    ======================================================================*/
+    findIndexOfTeam(teamName) {
+        for (let i in this.teamStandings) if (this.teamStandings[i].name === teamName) return i;
+        return false;
+    }
+
+    /*======================================================================
+    // This method will rearrange the teamStandings object of arrays 
+    // using two sorts. The first will sort alphabetically, while the
+    // second will sort by points.
+    ======================================================================*/
+    sortTeamStandings() {
+        this.teamStandings.sort((a, b) => a.name > b.name);
+        this.teamStandings.sort((a, b) => a.points < b.points);
+    }
+
+    /*======================================================================
+    // This method will output the current league standings for the
+    // user to see.
+    ======================================================================*/
+    displayLeagueStandings() {
+        console.log("\n===== CURRENT LEAGUE STANDINGS =====");
+        for (let i = 0; i < this.teamStandings.length; i++) {
+            console.log(`\n${i + 1}. ${this.teamStandings[i].name} - ${this.teamStandings[i].points} points`)
+        }
+        console.log("\n====================================");
+    }
 }
 
 module.exports = SoccerLeague;
--- src/app.js
+++ src/app.js
@@ -3,3 +3,6 @@ let SoccerLeague = require('../classes/SoccerLeague');
 let league1 = new SoccerLeague(); // Instantiate a new instance of SoccerLeague
 league1.readFileToString(); // Read the file into a class property
 league1.parseForTeamsAndScores(); // Parse string to determine teams and scores
+league1.calculateWinsPerTeam(); // Determine standings by creating an array of objects
+league1.sortTeamStandings(); // Sort standings
+league1.displayLeagueStandings(); // Display league standings
